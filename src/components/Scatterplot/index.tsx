17662d008624e81afbc2d6d61663c12c9ddd6bd3
Antônio Dias <antoniofdiass@gmail.com>
2023-06-04 22:28:31 -0300

    feat: add color attribute selection

--- src/components/Scatterplot/index.tsx
+++ src/components/Scatterplot/index.tsx
@@ -1,31 +1,70 @@
+import { useState } from 'react';
 import Plot from 'react-plotly.js';
 import { DataTableProps } from '../Table/types';
 
 export const ScatterPlot = ({ rows }: DataTableProps) => {
-  console.log(rows);
+  const [selectedColor, setSelectedColor] = useState<
+    | 'duration_ms'
+    | 'danceability'
+    | 'energy'
+    | 'loudness'
+    | 'speechiness'
+    | 'acousticness'
+    | 'instrumentalness'
+    | 'liveness'
+    | 'valence'
+    | 'tempo'
+  >('acousticness');
+
   return rows ? (
-    <Plot
-      data={[
-        {
-          x: rows.map((track) => track.x),
-          y: rows.map((track) => track.y),
-          mode: 'markers',
-          type: 'scatter',
-          // name: 'Team A',
-          text: rows.map((track) => track.name),
-          marker: {
-            size: 12,
-            color: rows.map((track) => track.colors.acousticness),
+    <div>
+      <Plot
+        config={{
+          displayModeBar: false,
+        }}
+        data={[
+          {
+            x: rows.map((track) => track.x),
+            y: rows.map((track) => track.y),
+            mode: 'markers',
+            type: 'scatter',
+            // name: 'Team A',
+            text: rows.map((track) => track.name),
+            marker: {
+              size: 12,
+              color: rows.map((track) => track.colors[selectedColor]),
+              colorscale: 'Viridis',
+              colorbar: {
+                title: selectedColor,
+              },
+              cmax: 1,
+              cmin: 0,
+            },
           },
-        },
-      ]}
-      layout={
-        {
-          // title: 'Data Labels Hover',
+        ]}
+        layout={
+          {
+            // title: 'Data Labels Hover',
+          }
         }
-      }
-      onClick={(event) => console.log(event.points)}
-    />
+        onClick={(event) => console.log(event.points)}
+      />
+      <div>
+        <label>Select color:</label>
+        <select
+          value={selectedColor}
+          onChange={(e) =>
+            setSelectedColor(e.target.value as typeof selectedColor)
+          }
+        >
+          {Object.keys(rows[0].colors).map((color, index) => (
+            <option key={index} value={color}>
+              {color}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
   ) : (
     <></>
   );
