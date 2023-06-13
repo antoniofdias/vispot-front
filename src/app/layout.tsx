fdbe5919ce2e72409f469172c23c4e516a9a8281
Antônio Dias <antoniofdiass@gmail.com>
2023-06-13 11:56:00 -0300

    build: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'TCC',
-  description: 'Eu só quero formar',
+  title: 'vispot',
+  description: 'A spotify playlist-based visualization app.',
 };
 
 export default function RootLayout({
